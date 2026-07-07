d8631ad52311444ff22f5df12529a91551fbd8a0
simranjitsit24-code <simranjits.it.24@nitj.ac.in>
2026-07-07 13:40:01 +0000

    Room controller

---
+++ backend/controllers/roomcontroller.js
@@ -0,0 +1,21 @@
+// Get all rooms with occupancy stats
+export const getRooms = async (req, res) => {
+  const rooms = await Room.find();
+  res.json(rooms);
+};
+
+// Add new room
+export const addRoom = async (req, res) => {
+  const { roomNumber, ward, totalBeds } = req.body;
+  const room = new Room({ roomNumber, ward, totalBeds, occupiedBeds: 0 });
+  await room.save();
+  res.status(201).json(room);
+};
+
+// Smart room suggestion
+export const suggestRoom = async (req, res) => {
+  const { ward } = req.query;
+  const room = await Room.findOne({ ward, occupiedBeds: { $lt: '$totalBeds' } }).sort({ occupiedBeds: 1 });
+  if (!room) return res.status(404).json({ error: 'No available room in this ward' });
+  res.json(room);
+};
